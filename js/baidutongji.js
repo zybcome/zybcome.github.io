07fe13196102df03e550ce01351cf8c90f13fb1f
zybcome <zybcome@sina.com>
2022-03-14 17:19:53 +0800

    提交

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a3be5ab26634acc19de079b4b1eaa520";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
